91c3b9dbc70a32329526bfb46225e51a72f08e03
pr-a-nav <pranavmishra680@gmail.com>
2024-07-15 20:25:11 +0530

    index

--- src/taint_s.rs
+++ src/taint_s.rs
@@ -1,7 +1,10 @@
 // use tantivy::collector::TopDocs;
 // use tantivy::query::QueryParser;
 use tantivy::schema::*;
-use tantivy;
+use tantivy::collector::TopDocs;
+use tantivy::indexer::{IndexWriter, SegmentWriter};
+use tantivy::{Document, IndexReader};
+use tantivy::index;
 
 pub fn schema_build(){
 
@@ -11,6 +14,15 @@ let content =schemabuilder.add_text_field("content", STRING);
 let repo_name = schemabuilder.add_text_field("Repo Name", STRING);
 let indexed = schemabuilder.add_bool_field("is indexed", STORED|FAST );
 
-schemabuilder.build();
+let schema = schemabuilder.build();
 println!("schemabuild");
+}
+
+pub fn index(s:String, schema:Schema){
+ let path_index = index::Index::create_in_dir(s,schema).unwrap();
+ let mutindex_writer:IndexWriter<TantivyDocument> = path_index.writer(1000000).unwrap();
+
+
+
+
 }
