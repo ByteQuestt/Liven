976b647caf3a363c5c4b76b28140a55182759939
pr-a-nav <pranavmishra680@gmail.com>
2024-07-16 18:22:10 +0530

    building indexing

--- src/taint_s.rs
+++ src/taint_s.rs
@@ -1,6 +1,6 @@
 // use tantivy::collector::TopDocs;
 // use tantivy::query::QueryParser;
-use tantivy::schema::*;
+use tantivy::{doc, schema::*};
 use tantivy::collector::TopDocs;
 use tantivy::indexer::{IndexWriter, SegmentWriter};
 use tantivy::{Document, IndexReader};
@@ -16,13 +16,29 @@ let indexed = schemabuilder.add_bool_field("is indexed", STORED|FAST );
 
 let schema = schemabuilder.build();
 println!("schemabuild");
+let file_name = schema.get_field("file_name").unwrap();
+let content = schema.get_field("content").unwrap();
+let repo_name= schema.get_field("repo_name").unwrap();
+let indexed = schema.get_field("indexed").unwrap();
+
 }
 
 pub fn index(s:String, schema:Schema){
- let path_index = index::Index::create_in_dir(s,schema).unwrap();
- let mutindex_writer:IndexWriter<TantivyDocument> = path_index.writer(1000000).unwrap();
-
-
+ let path_index = index::Index::create_in_dir(s,schema.clone()).unwrap();
+ let mut index_writer:IndexWriter<TantivyDocument> = path_index.writer(1000000).unwrap();
+ let file_name = schema.clone().get_field("file_name").unwrap();
+ let content = schema.get_field("content").unwrap();
+ let repo_name= schema.get_field("repo_name").unwrap();
+ let indexed = schema.get_field("indexed").unwrap();
+ index_writer.add_document( doc!(file_name => "example.ts",
+    content => " function double(x) {
+return x * 2;
+}",
+   repo_name=> "goodname",
+   indexed => true
+));
+ index_writer.commit();
+ 
 
 
 }
